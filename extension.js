4da97226817a2c91224dc7b98a1ef9a3fda22055
Rahul Gawale <rahul.g.jack@gmail.com>
2022-07-23 18:55:53 +0530

    restructure extnesion.js

--- extension.js
+++ extension.js
@@ -6,39 +6,31 @@ const layoutService = require('./src/layoutService');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let disposable = vscode.commands.registerCommand(
-        'lwr-snippets.createLwc',
-        function () {
-            lwcService.createLwc();
-        }
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'lwr-snippets.createLwc',
+            lwcService.createLwc
+        )
     );
-
-    context.subscriptions.push(disposable);
-
-    let disposableLayout = vscode.commands.registerCommand(
-        'lwr-snippets.createLayout',
-        function () {
-            layoutService.createLayout();
-        }
-    );
-
-    context.subscriptions.push(disposableLayout);
 
     context.subscriptions.push(
         vscode.commands.registerCommand(
             'lwr-snippets.createJsOnlyLwc',
-            function () {
-                lwcService.createJsOnlyLwc();
-            }
+            lwcService.createJsOnlyLwc
         )
     );
 
     context.subscriptions.push(
         vscode.commands.registerCommand(
             'lwr-snippets.createCssOnlyLwc',
-            function () {
-                lwcService.createCssOnlyLwc();
-            }
+            lwcService.createCssOnlyLwc
+        )
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'lwr-snippets.createLayout',
+            layoutService.createLayout
         )
     );
 }
